8d5a093662182f9f470db83c51d7784af0f36642
Matteo Redaelli <matteo.redaelli@gmail.com>
2020-10-12 17:30:28 +0200

    added more objects in schema

--- src/matteoredaelli/lacinia_backends_qliksense/backend.clj
+++ src/matteoredaelli/lacinia_backends_qliksense/backend.clj
@@ -8,26 +8,40 @@
    [clojure.string :as str])
   )
 
-(defn qliksense-config
+(defn get-qliksense-config
   []
   (->
    (System/getenv "LACINIA_BACKENDS_QLIKSENSE_CONFIG")
    slurp
    edn/read-string))
 
-(defonce config (qliksense-config))
+(defonce qliksense-config (get-qliksense-config))
 
 (defn get-objects-by-filter
-  [config system path filter-row]
-  (let [system_map (qliksense-core/get-system-map config system)
+  [qliksense-config system path filter-row]
+  (let [system_map (qliksense-core/get-system-map qliksense-config system)
         filter (if (= filter-row "") "1 eq 1" filter-row)
         params {"filter" filter}]
     (def resp (qliksense-api/get-request system_map path params))
     (resp :body)))
 
+(defn resolve-query-qliksense-get
+  [context arguments value path]
+  (let [{:keys [system filter]} arguments]
+    (get-objects-by-filter qliksense-config (keyword system) path filter)))
+
+(defn resolve-query-qliksense-custom-property-definitions
+  [context arguments value]
+  (resolve-query-qliksense-get context arguments value "/qrs/custompropertydefinition/full"))
+
+(defn resolve-query-qliksense-apps
+  [context arguments value]
+  (resolve-query-qliksense-get context arguments value "/qrs/app/full"))
+
 (defn resolve-query-qliksense-streams
   [context arguments value]
-  (let [{:keys [system filter]} arguments
-        path "/qrs/stream/full"]
+  (resolve-query-qliksense-get context arguments value "/qrs/stream/full"))
 
-    (get-objects-by-filter config (keyword system) path filter)))
+(defn resolve-query-qliksense-users
+  [context arguments value]
+  (resolve-query-qliksense-get context arguments value "/qrs/user/full"))
--- src/matteoredaelli/lacinia_backends_qliksense/schema.clj
+++ src/matteoredaelli/lacinia_backends_qliksense/schema.clj
@@ -11,6 +11,10 @@
   (-> (io/resource "schema.edn")
       slurp
       edn/read-string
-      (util/attach-resolvers {:resolve-query-qliksense-streams backend/resolve-query-qliksense-streams
+      (util/attach-resolvers {
+                              :resolve-query-qliksense-apps backend/resolve-query-qliksense-apps
+                              :resolve-query-qliksense-custom-property-definitions backend/resolve-query-qliksense-custom-property-definitions
+                              :resolve-query-qliksense-streams backend/resolve-query-qliksense-streams
+                              :resolve-query-qliksense-users backend/resolve-query-qliksense-users
                               })
       schema/compile))
